e65a0152d0ad42a9ca09ece561bd48326b1343a6
Guilherme De Castro Amaro <gude-cas@c1r15s15.42lisboa.com>
2024-05-05 16:14:37 +0100

    started project

--- inc/cub3D.h
+++ inc/cub3D.h
@@ -6,53 +6,21 @@
 /*   By: gude-cas <gude-cas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 15:27:56 by gude-cas          #+#    #+#             */
-/*   Updated: 2024/05/03 19:38:17 by gude-cas         ###   ########.fr       */
+/*   Updated: 2024/05/05 16:14:07 by gude-cas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# define KEY_ESC 53
-# define KEY_W 13
-# define KEY_A 0
-# define KEY_S 1
-# define KEY_D 2
-# define KEY_UP 126
-# define KEY_DOWN 125
-# define KEY_LEFT 123
-# define KEY_RIGHT 124
-
 # include <math.h>
 # include "libft/libft.h"
 # include "minilibx/mlx.h"
 
-typedef struct s_img
-{
-    int height;
-    int width;
-}   t_img;
-
-typedef struct s_player
-{
-    float x;
-    float y;    
-}   t_player;
-
-typedef struct s_ray
-{
-    float x;
-    float y;
-}   t_ray;
-
 typedef struct s_data
 {
     void *mlx;
-    void *mlx_win;
-    char **map;
-    t_img image;
-    t_ray ray;
-    t_player player;
+    void *win;
 }   t_data;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,42 +6,15 @@
 /*   By: gude-cas <gude-cas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 15:25:24 by gude-cas          #+#    #+#             */
-/*   Updated: 2024/04/29 19:40:35 by gude-cas         ###   ########.fr       */
+/*   Updated: 2024/05/05 16:13:18 by gude-cas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/cub3D.h"
 
-/* return 0 if file extension is correct */
-int	check_file_extension(char *filename)
+int main(void) 
 {
-	char	*extension;
-
-	extension = ft_strchr(filename, '.');
-	if (extension)
-	{
-		if (ft_strncmp(extension, ".cub", 5) == 0)
-			return (0);
-	}
-	return (1);
-}
-
-int close_window(t_data *data)
-{
-	mlx_destroy_window(data->mlx, data->mlx_win);
-	exit(0);
-}
-
-int main(int ac, char **av) 
-{
-	(void)ac;
-	(void)av;
-	t_data data;
-	
-	data.mlx = mlx_init();
-	data.mlx_win = mlx_new_window(data.mlx, 640, 480, "cub3D");
-	mlx_hook(data.mlx_win, 17, 0, close_window, &data);
-	mlx_loop(data.mlx);
+	printf("So it begins...\n");
 	
 	return (0);
 }
