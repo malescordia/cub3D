e9da3c1eebc50c52b5a40d6dc731fe495a15baa5
Camille Josephine Marie Bouvet <cbouvet@c1r15s15.42lisboa.com>
2024-05-27 17:11:14 +0100

    restrict mvt to cell bounds

--- cam.c
+++ cam.c
@@ -6,7 +6,7 @@
 /*   By: cbouvet <cbouvet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 16:59:04 by cbouvet           #+#    #+#             */
-/*   Updated: 2024/05/27 15:33:13 by cbouvet          ###   ########.fr       */
+/*   Updated: 2024/05/27 17:08:12 by cbouvet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,38 +15,48 @@
 int	hooks_handler(void)
 {
 	if (var()->left)
-	{
-		printf("left pressed\n");
 		var()->left = false;
-	}
 	if (var()->right)
-	{
-		printf("left pressed\n");
 		var()->left = false;
-	}
 	if (var()->w_key)
 	{
-		var()->player.pos[1] -= 0.1;
 		var()->w_key = false;
-		cube_mker(var()->map.cmap);
+		bound_checker(1, var()->disp.height, var()->player.pos[1] - 0.1);
 	}
 	if (var()->a_key)
 	{
-		var()->player.pos[0] -= 0.1;
 		var()->a_key = false;
-		cube_mker(var()->map.cmap);
+		bound_checker(0, var()->disp.width, var()->player.pos[0] - 0.1);
 	}
 	if (var()->s_key)
 	{
-		var()->player.pos[1] += 0.1;
 		var()->s_key = false;
-		cube_mker(var()->map.cmap);
+		bound_checker(1, var()->disp.height, var()->player.pos[1] + 0.1);
 	}
 	if (var()->d_key)
 	{
-		var()->player.pos[0] += 0.1;
 		var()->d_key = false;
-		cube_mker(var()->map.cmap);
+		bound_checker(0, var()->disp.width, var()->player.pos[0] + 0.1);
 	}
 	return (0);
+}
+
+void	bound_checker(int index, int axis, double target_pos)
+{
+	int	fl;
+	int	cl;
+
+	fl = floor(var()->player.pos[index]);
+	cl = ceil(var()->player.pos[index]);
+	printf("cl %i | content %i | pos %f | target %f\n", fl, var()->map.imap[index][cl], var()->player.pos[index], target_pos);
+	if (target_pos <= 0 || target_pos >= axis)
+		return ;
+	if (!var()->map.imap[index][fl] || !var()->map.cmap[index][cl])
+		return ;
+	if (var()->map.imap[index][fl] == 1 && target_pos <= fl)
+		return ;
+	if (var()->map.imap[index][cl] == 1 && target_pos >= cl)
+		return ;
+	var()->player.pos[index] = target_pos;
+	cube_mker(var()->map.cmap);
 }
